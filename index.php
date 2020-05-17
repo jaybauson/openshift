b315d2dc114ae7d9b13349d7c6aee8d9a815701d
Jay Bauson <bausonjc@ph.ibm.com>
2020-05-17 19:41:56 +0800

    Test Secret

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+#phpinfo();
+echo getenv('owm_api');
+echo "<hr/>";
 phpinfo();
-
+#05ac784e926abb5f7952bd4cb9fb73bb
+#api.openweathermap.org/data/2.5/weather?q=Marikina,Manila&units=metric&appid=05ac784e926abb5f7952bd4cb9fb73bb
 ?>
